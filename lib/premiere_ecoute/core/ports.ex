53db5e016db93876e2fb3dac0b57b1670ca2f6ea
Fly Dev <dev@fly.io>
2025-06-27 17:58:04 +0000

    fix ports file structure and warnings

--- lib/premiere_ecoute/core/ports.ex
+++ lib/premiere_ecoute/core/ports.ex
@@ -6,13 +6,32 @@ defmodule PremiereEcoute.Core.Ports do
 
   alias PremiereEcoute.Core.Entities
 
-  @doc """
-  Port for streaming platform integrations (Twitch, YouTube, etc.)
-  """
-  defmodule StreamingPlatformPort do
+  defmodule MusicMetadataPort do
+    @moduledoc """
+    Defines the interface for music metadata providers like Spotify.
     """
-    defmodule StreamingPlatformPort do
-    @moduledoc \"""
+
+    @type album_search_result :: %{
+            id: String.t(),
+            name: String.t(),
+            artist: String.t(),
+            release_date: Date.t() | nil,
+            cover_url: String.t() | nil,
+            total_tracks: integer()
+          }
+
+    @callback search_albums(query :: String.t()) ::
+                {:ok, [album_search_result()]} | {:error, term()}
+
+    @callback get_album_with_tracks(album_id :: String.t()) ::
+                {:ok, Entities.Album.t()} | {:error, term()}
+
+    @callback get_track_details(track_id :: String.t()) ::
+                {:ok, Entities.Track.t()} | {:error, term()}
+  end
+
+  defmodule StreamingPlatformPort do
+    @moduledoc """
     Defines the interface for streaming platform integrations.
     """
 
