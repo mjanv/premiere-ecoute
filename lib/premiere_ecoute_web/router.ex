3ff80453ecd697885c8df48874e026ea47ebedf7
Fly Dev <dev@fly.io>
2025-06-27 18:57:43 +0000

    remove registration and login routes, Twitch-only auth

--- lib/premiere_ecoute_web/router.ex
+++ lib/premiere_ecoute_web/router.ex
@@ -68,17 +68,11 @@ defmodule PremiereEcouteWeb.Router do
 
     live_session :current_user,
       on_mount: [{PremiereEcouteWeb.UserAuth, :mount_current_scope}] do
-      live "/users/register", UserLive.Registration, :new
-      live "/users/log-in", UserLive.Login, :new
-      live "/users/log-in/:token", UserLive.Confirmation, :new
       live "/", DashboardLive, :index
 
       # OAuth routes
       get "/auth/:provider", AuthController, :request
       get "/auth/:provider/callback", AuthController, :callback
     end
-
-    post "/users/log-in", UserSessionController, :create
-    delete "/users/log-out", UserSessionController, :delete
   end
 end
