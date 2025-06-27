dd0611b6ddbd2ba6bd3cf830beb6106a1c72e29a
Fly Dev <dev@fly.io>
2025-06-27 18:10:40 +0000

    move dashboard to current_user live_session

--- lib/premiere_ecoute_web/router.ex
+++ lib/premiere_ecoute_web/router.ex
@@ -67,6 +67,7 @@ defmodule PremiereEcouteWeb.Router do
       live "/users/register", UserLive.Registration, :new
       live "/users/log-in", UserLive.Login, :new
       live "/users/log-in/:token", UserLive.Confirmation, :new
+      live "/", DashboardLive, :index
     end
 
     post "/users/log-in", UserSessionController, :create
