fb67dfadf8ebf8a5d0f8636882bf6f695c4f721c
Fly Dev <dev@fly.io>
2025-06-27 17:56:41 +0000

    create hexagonal architecture ports

---
+++ lib/premiere_ecoute/core/ports.ex
@@ -0,0 +1,69 @@
+defmodule PremiereEcoute.Core.Ports do
+  @moduledoc """
+  Port definitions for hexagonal architecture.
+  These define the interfaces for external adapters.
+  """
+
+  alias PremiereEcoute.Core.Entities
+
+  @doc """
+  Port for music metadata services (Spotify, Apple Music, etc.)
+  """
+  defmodule MusicMetadataPort do
+    @moduledoc """
+    Defines the interface for fetching music metadata from external services.
+    """
+
+    @type search_result :: %{
+            id: String.t(),
+            name: String.t(),
+            artist: String.t(),
+            release_date: Date.t() | nil,
+            cover_url: String.t() | nil,
+            total_tracks: integer()
+          }
+
+    @callback search_albums(query :: String.t()) ::
+                {:ok, [search_result()]} | {:error, term()}
+
+    @callback get_album_with_tracks(album_id :: String.t()) ::
+                {:ok, Entities.Album.t()} | {:error, term()}
+
+    @callback get_track_details(track_id :: String.t()) ::
+                {:ok, Entities.Track.t()} | {:error, term()}
+  end
+
+  @doc """
+  Port for streaming platform integrations (Twitch, YouTube, etc.)
+  """
+  defmodule StreamingPlatformPort do
+    @moduledoc """
+    Defines the interface for streaming platform integrations.
+    """
+
+    @type poll_option :: %{text: String.t(), votes: integer()}
+    @type poll_result :: %{
+            id: String.t(),
+            question: String.t(),
+            options: [poll_option()],
+            status: :active | :ended,
+            total_votes: integer()
+          }
+
+    @callback authenticate_user(code :: String.t()) ::
+                {:ok, %{user_id: String.t(), access_token: String.t()}} | {:error, term()}
+
+    @callback create_poll(
+                channel_id :: String.t(),
+                question :: String.t(),
+                options :: [String.t()]
+              ) ::
+                {:ok, String.t()} | {:error, term()}
+
+    @callback get_poll_results(poll_id :: String.t()) ::
+                {:ok, poll_result()} | {:error, term()}
+
+    @callback listen_to_chat(channel_id :: String.t(), callback :: function()) ::
+                {:ok, pid()} | {:error, term()}
+  end
+end
