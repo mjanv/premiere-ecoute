131f3784f20cddb918c678cb0f284ebf5d21b783
Fly Dev <dev@fly.io>
2025-06-27 18:10:21 +0000

    add dashboard route to router

--- lib/premiere_ecoute_web/router.ex
+++ lib/premiere_ecoute_web/router.ex
@@ -1,6 +1,8 @@
 defmodule PremiereEcouteWeb.Router do
   use PremiereEcouteWeb, :router
 
+  import PremiereEcouteWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule PremiereEcouteWeb.Router do
     plug :put_root_layout, html: {PremiereEcouteWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -17,7 +20,7 @@ defmodule PremiereEcouteWeb.Router do
   scope "/", PremiereEcouteWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", DashboardLive, :index
   end
 
   # Other scopes may use custom stacks.
@@ -40,5 +43,33 @@ defmodule PremiereEcouteWeb.Router do
       live_dashboard "/dashboard", metrics: PremiereEcouteWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", PremiereEcouteWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{PremiereEcouteWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", PremiereEcouteWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{PremiereEcouteWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
