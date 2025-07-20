f0a0520299f836148aaf6ea04c65958e3b58b951
Maxime Janvier <maxime.janvier@hpe.com>
2025-07-20 20:53:43 +0200

    wip

--- lib/premiere_ecoute_web.ex
+++ lib/premiere_ecoute_web.ex
@@ -17,7 +17,7 @@ defmodule PremiereEcouteWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(doc assets fonts images favicon.ico robots.txt)
 
   def router do
     quote do
