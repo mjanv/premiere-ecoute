24d3a211f67ddee8b1b7a8baabc118c21b5874a6
Maxime Janvier <maxime.janvier@hpe.com>
2025-08-02 13:40:13 +0200

    profile

--- lib/premiere_ecoute/accounts/user/profile.ex
+++ lib/premiere_ecoute/accounts/user/profile.ex
@@ -5,13 +5,19 @@ defmodule PremiereEcoute.Accounts.User.Profile do
 
   import Ecto.Changeset
 
+  @schemes [:light, :dark, :system]
+  @languages [:en, :fr, :it]
+
   embedded_schema do
-    field :display_name, :string
+    field :color_scheme, Ecto.Enum, values: @schemes, default: :system
+    field :language, Ecto.Enum, values: @languages, default: :en
   end
 
   def changeset(profile, attrs \\ %{}) do
     profile
-    |> cast(attrs, [:display_name])
-    |> validate_required([:display_name])
+    |> cast(attrs, [:color_scheme, :language])
+    |> validate_required([:color_scheme, :language])
+    |> validate_inclusion(:color_scheme, @schemes)
+    |> validate_inclusion(:language, @languages)
   end
 end
--- test/premiere_ecoute/accounts/user/profile_test.exs
+++ test/premiere_ecoute/accounts/user/profile_test.exs
@@ -8,33 +8,33 @@ defmodule PremiereEcoute.Accounts.User.ProfileTest do
     test "can create an user with a default user profile" do
       {:ok, user} = User.create(%{email: "user@email.com"})
 
-      assert %Profile{id: _, display_name: nil} = user.profile
+      assert %Profile{id: _, color_scheme: :system, language: :en} = user.profile
     end
 
     test "can create an user with an user profile" do
-      {:ok, user} = User.create(%{email: "user@email.com", profile: %{display_name: "User"}})
+      {:ok, user} = User.create(%{email: "user@email.com", profile: %{color_scheme: :light, language: :it}})
 
-      assert %Profile{id: _, display_name: "User"} = user.profile
+      assert %Profile{id: _, color_scheme: :light, language: :it} = user.profile
     end
   end
 
   describe "update/1" do
     test "can update an user with an user profile" do
-      {:ok, user} = User.create(%{email: "user@email.com", profile: %{display_name: "User"}})
+      {:ok, user} = User.create(%{email: "user@email.com", profile: %{color_scheme: :light, language: :it}})
 
-      {:ok, user} = User.update(user, %{email: "user2@email.com", profile: %{display_name: "New User"}})
+      {:ok, user} = User.update(user, %{email: "user2@email.com", profile: %{color_scheme: :dark, language: :en}})
 
-      assert %Profile{id: _, display_name: "New User"} = user.profile
+      assert %Profile{id: _, color_scheme: :dark, language: :en} = user.profile
     end
   end
 
   describe "edit_user_profile/1" do
     test "can update an user with an user profile" do
-      {:ok, user} = User.create(%{email: "user@email.com", profile: %{display_name: "User"}})
+      {:ok, user} = User.create(%{email: "user@email.com", profile: %{color_scheme: :light, language: :it}})
 
-      {:ok, user} = User.edit_user_profile(user, %{display_name: "New User"})
+      {:ok, user} = User.edit_user_profile(user, %{color_scheme: :dark, language: :en})
 
-      assert %Profile{id: _, display_name: "New User"} = user.profile
+      assert %Profile{id: _, color_scheme: :dark, language: :en} = user.profile
     end
   end
 end
