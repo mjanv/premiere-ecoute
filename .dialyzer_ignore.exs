eaf356afccb701b65b0736cdf4a11e28d80a0a0a
Maxime Janvier <maxime.janvier@gmail.com>
2026-03-28 19:11:09 +0100

    Add translations for duel and player UI features
    Extract and translate new strings from recent UI improvements: player status display, duel mode enhancements, and picker restore functionality. Support French and Italian.
    
    ## Features
    
    - French translations: Enrichissement, Rien en lecture, Garder les deux, Restaurer
    - Italian translations: Arricchimento, Nessuna riproduzione, Tieni entrambi, Ripristina
    - Update fuzzy translation for enrichment failure message

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -38,7 +38,7 @@
   {"lib/premiere_ecoute/radio/radio_track.ex", :call_without_opaque},
   {"lib/premiere_ecoute_web/components/core_components.ex", :call_without_opaque},
   {"lib/premiere_ecoute_web/components/cards/modal.ex", :call_without_opaque},
-  {"lib/premiere_ecoute_web/components/panels/wikipedia_drawer.ex", :pattern_match},
+  # {"lib/premiere_ecoute_web/components/panels/wikipedia_drawer.ex", :pattern_match},
   {"lib/premiere_ecoute_web/layouts/layouts.ex", :call_with_opaque},
   {"lib/premiere_ecoute_web/components/panels/drawer.ex", :call_without_opaque}
 ]
