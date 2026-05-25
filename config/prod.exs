1d6cb702005964c06002d335d629ec0bd7335fbc
Maxime Janvier <maxime.janvier@gmail.com>
2026-05-25 23:26:33 +0200

    fix: image proxy cache survives deploys
    Configure the image proxy controller to use a persistent cache directory on the server that won't be wiped by rsync during deployments.
    
    ## Bugs
    
    - Image proxy cache was stored in the build directory and deleted on every deploy, causing 500 errors on cache misses
    - Added rsync --exclude='cache' to preserve the cache across deployments

--- config/prod.exs
+++ config/prod.exs
@@ -5,6 +5,8 @@ config :premiere_ecoute,
   sentry: "https://maxime-janvier.sentry.io/insights/projects/premiere-ecoute/?project=4509617392975872",
   grafana: "https://mjanv.grafana.net/dashboards/f/B412468D664E3FDF89E566662E1950E3/?orgId=1"
 
+config :premiere_ecoute, PremiereEcouteWeb.Images.ImageProxyController, cache_dir: "/opt/premiere-ecoute/cache/images/proxy"
+
 config :premiere_ecoute, PremiereEcoute.Repo,
   ssl: false,
   pool_size: 2
