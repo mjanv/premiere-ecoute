6df94b787115fdb03fbd614ffabc22937af7cc22
Maxime Janvier <maxime.janvier@gmail.com>
2026-06-20 14:26:14 +0200

    chore: upgrade PostgreSQL to 18.4
    Bump local Docker Compose PostgreSQL image from 17.6 to 18.4 and update volume mount path to match PG18+ expectations. Add backup files to gitignore.
    
    ## Improvements
    
    - Update postgres image to 18.4
    - Change volume mount from `/var/lib/postgresql/data` to `/var/lib/postgresql` (required for PG18+)
    - Add `*.sql.gz` to gitignore for database backups
    - Minor refactoring in tracking module (destructuring, capitalization)

--- lib/premiere_ecoute/analytics/tracking.ex
+++ lib/premiere_ecoute/analytics/tracking.ex
@@ -1,21 +1,18 @@
 defmodule PremiereEcoute.Analytics.Tracking do
   @moduledoc """
-  PostHog event tracking. All functions accept a User struct as first argument.
+  PostHog event tracking.
   """
 
   alias PremiereEcoute.Accounts.User
   alias PremiereEcoute.Discography.Album
 
   @spec album_viewed(User.t(), Album.t()) :: :ok
-  def album_viewed(%User{id: user_id}, %Album{} = album) do
-    capture("album_viewed", user_id,
-      album_id: album.id,
-      album_name: album.name
-    )
+  def album_viewed(%User{id: user_id}, %Album{id: album_id, name: album_name}) do
+    capture("album_viewed", user_id, album_id: album_id, album_name: album_name)
   end
 
   defp capture(event, user_id, properties) do
-    Posthog.capture(event, [distinct_id: user_id] ++ properties, [])
+    PostHog.capture(event, [distinct_id: user_id] ++ properties, [])
     :ok
   end
 end
